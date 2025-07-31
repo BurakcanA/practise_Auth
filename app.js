763f5c11edd7e13f2c3a7992480b64060232b66c
Burakcan <burakcanaytac@gmail.com>
2025-07-31 17:06:20 +0300

    feature/Session &Login &logout

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const app = express()
 const path = require('path')
 const User = require('./models/user')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}))
+app.use(session({
+    secret: 'MakeThisSecretBetter',
+    // resave: false,
+    // saveUninitialized: true,
+    // cookie: { secure: true }
+}))
 
 const db = mongoose.connection;
 mongoose.connect('mongodb://127.0.0.1:27017/Auth')
@@ -16,7 +23,14 @@ db.on('error', err => console.log(`Connection Failed: ${err}`))
 db.once('open',() => console.log('Database Connected!'))
 
 app.get('/secret', (req,res) => {
-    res.send('This is SECRET')
+    if(req.session.user_id != null)
+    {
+        res.send('You are still logged in and this is result')
+    }
+    else
+    {
+        res.send(`You can't see the SECRET unles you are logged in`)
+    }
 })
 
 app.get('/register', (req, res) => {
@@ -39,12 +53,18 @@ app.post('/login', async (req,res) => {
     const result = await bcrypt.compare(password, user.password)
     if(result)
     {
+        req.session.user_id = user._id
         res.send('Logged In')
     }else
     {
         res.send('Failed')
     }
     
+})
+
+app.get('/logout', (req, res) => {
+    req.session.user_id = null
+    res.send('You are logouted')
 })
 
 app.listen('3000', (req,res) => {
